fb309ffc8e1c0d06193d9d9cd4a6005b7f3a9ed4
Paul Thompson <paul.j.m.thompson@gmail.com>
2016-10-17 21:20:27 +0100

    Initial Solution creation.

---
+++ src/WebAdmin.Infrastructure/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebAdmin.Infrastructure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebAdmin.Infrastructure")]
+[assembly: AssemblyCopyright("Copyright © LogicMelon 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("766eafc4-52a5-4471-8fcb-b8b5e665aa92")]
